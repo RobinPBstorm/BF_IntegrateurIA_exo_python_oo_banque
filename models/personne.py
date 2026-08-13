68cd0b0fb4d0733f438471b5efc3c673bd8a5fcb
robinPecheurBstorm <robin.pecheur@bstorm.be>
2026-08-13 10:25:23 +0200

    Demo de proptiétés

--- models/personne.py
+++ models/personne.py
@@ -4,6 +4,17 @@ class Personne:
         self.nom = nom
         self.prenom = prenom
 
+    def nom_comlet2(self):
+        return f"{self.prenom} {self.nom}"
+
+    @property
+    def nom_complet(self):
+        return f"{self.prenom} {self.nom}"
+
 if __name__ == "__main__":
     john_doe = Personne(1, "Doe", "John")
     print(john_doe.prenom)
+    print(john_doe.nom_complet)
+    # n'est pas possible car pas de setter
+    #john_doe.nom_complet = "Jane Doe"
+    print(john_doe.nom_comlet2())
